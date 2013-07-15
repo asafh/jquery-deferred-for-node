b760daeb50627ffa79a63d7f034b243e85d522fe
Asafh <asafh.gh@ous.im>
2013-07-15 21:13:29 +0300

    Removed usage of plus

--- import/src_core.js
+++ import/src_core.js
@@ -1,4 +1,4 @@
-require( "plus" );
+var _ = require("lodash");
 
 var r_decl = /(\s+)(_?Deferred|when)\:((?:.|\n)+?)\1},?/g,
 	r_vars = /(?:\s+)(slice|promiseMethods|core_slice|core_rspace|core_rnotwhite) =([^,]+),/g,
@@ -19,5 +19,5 @@ module.exports = function( code ) {
 	code.replace( r_decl, function( _, __, name, body ) {
 		defs[ name ] = ( name === "Deferred" ? "var Deferred = module.exports" : "Deferred." + name ) + " =" + body + "\n};";
 	});
-	return defs.join( "\n\n" );
+	return _.map(defs).join( "\n\n" );
 };
